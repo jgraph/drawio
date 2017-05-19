b031d33a9afcca4330b0183db027469b7858bf8e
Gaudenz Alder <gaudenz@jgraph.com>
2017-05-19 16:27:06 +0200

    This shouldn’t have been pushed
    Former-commit-id: da406904d43a549febc71c79fd791a984f7beb44

--- etc/electron/node_modules/concat-stream/node_modules/readable-stream/writable.js
+++
@@ -1,8 +0,0 @@
-var Stream = require("stream")
-var Writable = require("./lib/_stream_writable.js")
-
-if (process.env.READABLE_STREAM === 'disable') {
-  module.exports = Stream && Stream.Writable || Writable
-}
-
-module.exports = Writable
