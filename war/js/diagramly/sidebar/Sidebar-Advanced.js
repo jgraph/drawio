c1773c5677b909c372029100c12f86208ad53b41
Gaudenz Alder <gaudenz@jgraph.com>
2017-08-31 20:51:47 +0200

    7.3.4 release
    Former-commit-id: e5240113f4605913f74253517c09383647653515

--- war/js/diagramly/sidebar/Sidebar-Advanced.js
+++ war/js/diagramly/sidebar/Sidebar-Advanced.js
@@ -12,7 +12,7 @@
 
 		// Reusable cells
 		var flow = new mxCell('Vertical Flow Layout', new mxGeometry(0, 0, 270, 280),
-				'swimlane;html=1;startSize=20;horizontal=1;childLayout=flowLayout;flowOrientation=north;resizable=0;interRankCellSpacing=50;');
+				'swimlane;html=1;startSize=20;horizontal=1;childLayout=flowLayout;flowOrientation=north;resizable=0;interRankCellSpacing=50;containerType=tree;');
 		flow.vertex = true;
 		
 		var flow1 = new mxCell('Start', new mxGeometry(20, 20, 100, 40), 'whiteSpace=wrap;html=1;');
@@ -64,9 +64,9 @@
 			this.addDataEntry('container swimlane pool vertical', 380, 480, 'Vertical Pool 2',
 				'xZTPbsIwDMafJvf86dDOlI0LSEg8QUQtEi1pUBJGy9PPbdJ1G1TqhXGoZH/219g/RSGitM3ay5PaugoMEW9ElN65mCLblGAM4VRXRKwI5xQ/wt8nqqyv0pP0UMc5Bp4Mn9KcISk750wSQ2xNFsNFWyNrzJYqWpxyxTA8KG2qjWzduTsrRHn4GLKlh6CvsBsGYX+krWxQpaiizcc9FjDnnaCc11dXR2lyxyjsuyPy3/Lg4CM0k8v3Ut58Dc5C9C22XHQVVeoQrwkQVaCPKtuKQZQhCcdv78gSg4zzPlpxg3bTEeSUzcR7Q2bWyvz+ytmQr8NPAow/ikAxRYA/kQAr/hPByxQC8cxLsHggAkzH56uv/XrdvgA='),
 			this.addDataEntry('vertical tree layout', 280, 190, 'Vertical Tree Layout',
-				'5ZXNUoMwEICfJncgteq1UHvRi3W8p2QHMhNIJyyl9OndQLBFWqcz6kE9MGT/s182E8bjYr+yYps/GQma8SXjsTUG+1Wxj0FrFgVKMp6wKAroY9HDBWvYWYOtsFDiNQFRH7ATuoZe8woWVSqc04sFoN+jaE2NvWOFrfaOVaMKLUqSFjkWtPMkpGWFwuJaHZwT1SabsepgShSDR5orLX1OniDVGIQT3640T1wCC5U6iI0eZL9j2ibsL3bdqXzLKzAFoG3JpVESc9/5XU8myEFl+RB275Wi6hXZe+wRIi08x/NM+YTpszvPj/yaXCGstyJ1ckMTMAZ5VZvR+TZ9gD/7oB2LJxDC4AyE2TcwmE0YxO7YSRX+LxA3ExCT/kFmsPYi6I1plkfFolOQYTdcyxEba+pSghzdDJfuc05U3dQ2hdG40r3NAE9Ob0rTghaoduPsX4EzvzglU0p/ekpuf+OUzH9mSkg8vn+dbfQ8vgE='),
+				'5ZXNUoMwEICfhjuQWvVaqL3oxXa8p7ADmQmECUspfXo3JNgirdMZ7UE9MGT/s182E49FxX6leZW/qBSkx5Yei7RSaFfFPgIpvdAXqcdiLwx9+rzw6YI16K1+xTWUeE1AaAN2XDZgNW+gUSTcOG00AP2eeacatI41dtI51q0oJC9JWuRY0M7jgJY1co1rcTBOVJtsSouDKpEPHkkuZOpyshipxiCc+PalWWwSaKjFgW/lICfGQZSgN10FLgOpXSO0e9hfhNGrHIkVqAJQd+TSihRzB+TBAvNzEFk+hD06Ja+tIvuIPbKlhcN7HjWboH41x/wZa5sLhHXFEyO3NBhjvle1GZ5v0wW4kfC7sXgCIfDPQJj9AIPZhEFkpoFUwf8CcTcBMekf0gzWTgS5Ve3yqFj0CjLshts6YqNVU6aQugtjM5t0X3Oi6qrRCYzGla5zBnhyelOaGiRHsRtn/w6c+cUpmVL601Ny/xunZH6bKSHx+Cz2ttGr+Q4='),
 			this.addDataEntry('horizontal tree layout', 310, 160, 'Horizontal Tree Layout',
-				'5ZXNUoMwEICfhjuQWu+FWg96sb5A2uxAZgJhwlJKn94NSQWkdTqjHtQDQ/Y/+2UzCVhSHDeGV/mzFqACtg5YYrRGtyqOCSgVxKEUAUuDOA7pC+KHK9aot4YVN1DiLQGxCzhw1YDTPGojT7pEbt1eDQD9nninG3SuNXbKu9atLBQvSVrlWNDe04iWNXKDW3myTlSdbENGllrFPpdK+JwsRapxFka+fWmX0UAtT3zXl7Xxfs9gEI5X++5VvukN6ALQdOTSSoG582CRYxPmILP8HLb0Sl47RfYeO2CkhSd5mSqbUX2xJ/qRX5tLhG3F91ZuaQamIG9qM77cpg/wpx92U3EEIQovQFh8A4PFjEFij51U0f8CcTcDMesfRAZbL4La6XY9KFa9ggzjazSmY3RTChCTu2ETfk6K6uvG7GEysHRzM8DR+c15GlAc5WGa/St4llfnZM7pT8/J/e+ck+XPzAmJwyvY2yaP5Bs='),
+				'5ZXNUoMwEMefJncgtd4LVQ96sX2BtOxAZgJhwlJKn94NSQWkdTqjHtQDQ/a/X+THZsJ4XBwfjajyF52CYnzNeGy0RrcqjjEoxaJApownLIoCelj0cMUb9t6gEgZKvCUhcgkHoRpwypM28qRLFDZsawDo9Sw63aALrbFTPrRuZaFESdYqx4K+PQlpWaMwuJEnG0TdyTdU5IkV9rlUqa/JE6QeZ2MU27d2FQ3U8iR2fds+3wbIEsy2q8BXINlvBQzC8SqOXvIsHkEXgKajkFammLsIHjpkQQ4yy89pSy+K2gnZe+5AlxYe8GXYfAb71f7oj1jbXCJsKrG3dkujMeV70zajy9v0CX4ogm5qjiCEwQUIi29gsJgxiO00kBT+LxB3MxCz/UOawcaboHa6XQ/CqhfIMT5dYzpGN2UKqT8yrrYt+Dkp6q8bs4fJwNKBzgBH/2/O04ASKA/T6l/Bs7w6J3NOf3pO7n/nnCx/Zk7IHC7H3je5O98A'),
 				
 		 	this.addEntry('vertical flow layout', function()
 			{
@@ -76,7 +76,7 @@
 			{
 				var cell = sb.graph.cloneCells([flow])[0];
 				cell.geometry = new mxGeometry(0, 0, 460, 150);
-				cell.style = 'swimlane;html=1;startSize=20;horizontal=0;childLayout=flowLayout;flowOrientation=west;resizable=0;interRankCellSpacing=50;';
+				cell.style = 'swimlane;html=1;startSize=20;horizontal=0;childLayout=flowLayout;flowOrientation=west;resizable=0;interRankCellSpacing=50;containerType=tree;';
 				cell.value = 'Horizontal Flow Layout';
 				
 				return sb.createVertexTemplateFromCells([cell], cell.geometry.width, cell.geometry.height, 'Horizontal Flow Layout', true);
