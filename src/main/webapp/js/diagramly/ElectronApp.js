db518f94dd818e7ce54bea1a7898c8ebd4ca97c8
Gaudenz Alder <gaudenz@jgraph.com>
2019-06-03 16:22:11 +0200

    10.7.4 release
    Former-commit-id: 65577e2b2bcfc5ed5bff10939bfd140645fd1d07

--- src/main/webapp/js/diagramly/ElectronApp.js
+++ src/main/webapp/js/diagramly/ElectronApp.js
@@ -1,6 +1,6 @@
 window.OPEN_URL = 'https://www.draw.io/open';
 window.TEMPLATE_PATH = 'templates';
-window.DRAW_MATH_URL = 'mathjax/src/main/webapp/current';
+window.DRAW_MATH_URL = 'https://www.draw.io/math';
 FeedbackDialog.feedbackUrl = 'https://log.draw.io/email';
 
 (function()
