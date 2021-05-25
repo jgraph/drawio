751b3bcba966b30c7152997d84f96d8657bb6c1c
David Benson [draw.io] <david@jgraph.com>
2021-05-25 12:39:22 +0100

    14.7.1 release

--- src/main/java/com/mxgraph/online/SaveServlet.java
+++ src/main/java/com/mxgraph/online/SaveServlet.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.mxgraph.util.mxBase64;
-
 /**
  * Servlet implementation class SaveServlet
  */
