01ccb271d34258872b859c0fc1d253cc81341917
David Benson <david@draw.io>
2022-05-15 11:13:37 +0100

    Limit wellknown servlet to serve single file

--- src/main/java/com/mxgraph/online/WellKnownServlet.java
+++ src/main/java/com/mxgraph/online/WellKnownServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet to fake a .well-known directory
+ * Servlet to fake a .well-known directory, GAE does not directly support . prefixed directories
  */
 @SuppressWarnings("serial")
 public class WellKnownServlet extends HttpServlet
@@ -38,31 +38,41 @@ public class WellKnownServlet extends HttpServlet
 	{
         // GAE can't serve dot prefixed folders
         String uri = request.getRequestURI().replace("/.", "/");
-
-        if (uri.toLowerCase().contains(".json"))
+        
+        // Currently, there is only one file that this servlet serves. This is only
+        // needed if you want OneDrive integration. 
+        if (uri != null && uri.equals("/well-known/microsoft-identity-association.json"))
         {
-        	response.setContentType("application/json");
+            if (uri.toLowerCase().contains(".json"))
+            {
+                response.setContentType("application/json");
+            }
+
+            // Serve whatever was requested from .well-known
+            try (InputStream in = getServletContext().getResourceAsStream(uri))
+            {
+                if (in == null)
+                {
+                    response.sendError(404);
+                    return;
+                }
+                
+                byte[] buffer = new byte[8192];
+                int count;
+
+                while ((count = in.read(buffer)) > 0)
+                {
+                    response.getOutputStream().write(buffer, 0, count);
+                }
+                
+                response.getOutputStream().flush();
+                response.getOutputStream().close();
+            }
         }
-
-        // Serve whatever was requested from .well-known
-        try (InputStream in = getServletContext().getResourceAsStream(uri))
+        else
         {
-            if (in == null)
-            {
-            	response.sendError(404);
-                return;
-            }
-            
-            byte[] buffer = new byte[8192];
-            int count;
-
-            while ((count = in.read(buffer)) > 0)
-            {
-            	response.getOutputStream().write(buffer, 0, count);
-            }
-            
-            response.getOutputStream().flush();
-            response.getOutputStream().close();
+            response.sendError(404);
+            return;
         }
     }
 }
